aa7a4b5252a124fba6d645890163a27af5d1eccb
ksolis <limonsito.kls@hotmail.com>
2024-03-05 02:56:23 +0100

    mi first apiRest without mysql

--- src/main/java/com/limoncitoscompany/buildingrestfull/controller/ProductController.java
+++ src/main/java/com/limoncitoscompany/buildingrestfull/controller/ProductController.java
@@ -6,12 +6,15 @@ import com.limoncitoscompany.buildingrestfull.model.Product;
 import com.limoncitoscompany.buildingrestfull.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Arrays;
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 public class ProductController {
@@ -25,5 +28,20 @@ public class ProductController {
     @GetMapping("/products/{pId}")
     public Product getProduct(@PathVariable("pId") String id) {
         return productService.getProduct(id);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product){
+        productService.addProduct(product);
+    }
+
+    @PutMapping("products/{pId}")
+    public void updateProduct(@RequestBody Product product,@PathVariable("pId") String id ) {
+                productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("products/{pId}")   
+    public void deleteProduct(@PathVariable("pId") String id ) {
+        productService.deleteProduct(id);
     }
 }
--- src/main/java/com/limoncitoscompany/buildingrestfull/service/ProductService.java
+++ src/main/java/com/limoncitoscompany/buildingrestfull/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.limoncitoscompany.buildingrestfull.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,6 +10,10 @@ import com.limoncitoscompany.buildingrestfull.model.Product;
 
 @Service
 public class ProductService {
+    /*
+    El problema que estás experimentando se debe a que estás utilizando Arrays.asList, 
+    que devuelve una lista fija que no admite operaciones de modificación como agregar 
+    o quitar elementos. Por lo tanto, no puedes agregar nuevos elementos a esta lista.
     private List<Product> products = Arrays.asList(
         new Product("p101","Monitor","Electronics"),
         new Product("p102","Monitor","Electronics"),
@@ -16,12 +21,40 @@ public class ProductService {
         new Product("p104","Monitor","Electronics"),
         new Product("p105","Monitor","Electronics"),
         new Product("p106","Monitor","Electronics"));
-  
+     */
+
+
+    private List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product("p101","Monitor","Electronics"),
+            new Product("p102","Monitor","Electronics"),
+            new Product("p103","Monitor","Electronics"),
+            new Product("p104","Monitor","Electronics"),
+            new Product("p105","Monitor","Electronics"),
+            new Product("p106","Monitor","Electronics")));
+
+
     public List<Product> getAllProducts() {
         return products;
     }
 
     public Product getProduct(String id){
         return products.stream().filter(p -> p.getId().equals(id)).findFirst().get();
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void updateProduct(String id, Product product){
+        for (int i = 0; i < products.size(); i++) {
+            Product p = products.get(i);
+            if(p.getId().equals(id)){
+                products.set(i,product);
+            }
+        }
+    }
+
+    public void deleteProduct(String id){
+            products.removeIf(p->p.getId().equals(id));
     }
 }
